8d14a70bfe645dd0cc915b7991b3d531f1782eaa
Lucas Camilo - ZUP <lucas.camilo@zup.com.br>
2021-02-25 09:18:29 -0300

    Initial Commit

---
+++ PocDynatraceIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PocDynatraceIOS
+//
+//  Created by Lucas Camilo on 25/02/21.
+//  Copyright © 2021 Lucas Camilo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
